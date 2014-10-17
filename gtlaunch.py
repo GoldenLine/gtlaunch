5310cacfdd7be6630b23ae87f7aa74233268c4f6
Zbigniew Siciarz <antyqjon@gmail.com>
2014-10-17 21:56:49 +0200

    First version.

---
+++ gtlaunch.py
@@ -0,0 +1,42 @@
+#/usr/bin/env python
+
+import subprocess
+
+config = {
+    'test': {
+        'prefix': 'workon test',
+        'cwd': '~/Development',
+        'tabs': [
+            {
+                'title': 'editor',
+                'command': 'vim',
+            },
+            {
+                'title': 'python',
+                'command': 'python',
+            },
+            {
+                'title': 'git',
+                'command': 'git status',
+            },
+        ],
+    },
+}
+
+
+def run():
+    project = config['test']
+    args = ['gnome-terminal', '--maximize']
+    args.extend(['--working-directory', project['cwd']])
+    for idx, tab in enumerate(project['tabs']):
+        tab_option = '--tab' if idx == 0 else '--tab-with-profile=Default'
+        prefix = project.get('prefix', 'true')
+        command = "zsh -is eval '{} && {}'".format(prefix, tab['command'])
+        args.append(tab_option)
+        args.extend(['--title', tab['title']])
+        args.extend(['--command', command])
+    return subprocess.Popen(args)
+
+
+if __name__ == "__main__":
+    run()
