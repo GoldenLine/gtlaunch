0f183708aa8a0c9503d847a65f072de07dc800ea
Zbigniew Siciarz <antyqjon@gmail.com>
2014-10-18 11:34:23 +0200

    Check for maximize in args.

--- tests.py
+++ tests.py
@@ -16,6 +16,16 @@ class LauncherTestCase(unittest.TestCase):
         launcher = Launcher(options, lazy=True)
         self.assertIsNone(launcher.project)
 
+    def test_args_maximize(self):
+        options = MockOptions()
+        project = {
+            'cwd': '~',
+            'tabs': [],
+        }
+        launcher = Launcher(options, lazy=True)
+        args = launcher.build_args(project)
+        self.assertIn('--maximize', args)
+
 
 if __name__ == '__main__':
     unittest.main()
